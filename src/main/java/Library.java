dd3efb2b502b3af1554f377178e2cd1088058036
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-26 17:31:36 +0000

    Created classes for Book and Library, intial test classes and check for empty start collection

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> bookCollection;
+
+    public Library(){
+        this.bookCollection = new ArrayList<>();
+    }
+
+    public int getNumberOfBooksInCollection(){
+        return bookCollection.size();
+    }
+
+
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book1, book2, book3;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book("Anarch", "Dan Abnett", "Science Fiction");
+        book2 = new Book("13th Legion", "Guy Thorpe", "Science Fiction");
+        book3 = new Book("Necronomicon", "H. P. Lovecraft", "Horror");
+    }
+
+    @Test
+    public void checkCollectionStartsEmpty(){
+        assertEquals(0, this.library.getNumberOfBooksInCollection());
+    }
+
 }
