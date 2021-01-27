599e7688a4238e07372ab2141acaa2677330b181
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-27 09:53:54 +0000

    Added getters and setters for all three  Book properties

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -9,7 +9,27 @@ public class Book {
         this.genre = genre;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     public String getGenre(){
         return this.genre;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 }
